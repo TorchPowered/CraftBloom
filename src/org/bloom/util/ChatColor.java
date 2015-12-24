7fc312f17cf2cd29319033bbf5bbd1572de06e63
PizzaCrust <torchprojectserver@gmail.com>
2015-12-24 18:49:47 +0700

    Update to the latest patch of ChatColor.

---
+++ src/org/bloom/util/ChatColor.java
@@ -0,0 +1,90 @@
+package org.bloom.util;
+
+/**
+ * Represents color in the chat
+ */
+public class ChatColor {
+    /**
+     * The minecraft color symbol
+     */
+    public static char COLOR_SYMBOL = '\u00A7';
+    /**
+     * Represents the color black
+     */
+    public static String BLACK = COLOR_SYMBOL + "0";
+
+    /**
+     * Represents the color of dark blue
+     */
+    public static String DARK_BLUE = COLOR_SYMBOL + "1";
+
+    /**
+     * Represents the color of dark green
+     */
+    public static String DARK_GREEN = COLOR_SYMBOL  + "2";
+
+    /**
+     * Represents the color of dark aqua
+     */
+    public static String DARK_AQUA = COLOR_SYMBOL + "3";
+
+    /**
+     * Represents the color of dark red
+     */
+    public static String DARK_RED = COLOR_SYMBOL + "4";
+
+    /**
+     * Represents the color of dark purple
+     */
+    public static String DARK_PURPLE = COLOR_SYMBOL + "5";
+
+    /**
+     * Represents the color of gold
+     */
+    public static String GOLD = COLOR_SYMBOL + "6";
+
+    /**
+     * Represents the color of gray
+     */
+    public static String GRAY = COLOR_SYMBOL + "7";
+
+    /**
+     * Represents the color of dark gray
+     */
+    public static String DARK_GRAY = COLOR_SYMBOL + "8";
+
+    /**
+     * Represents the color of blue
+     */
+    public static String BLUE = COLOR_SYMBOL + "9";
+
+    /**
+     * Represents the color of green
+     */
+    public static String GREEN = COLOR_SYMBOL + "a";
+
+    /**
+     * Represents the color of aqua
+     */
+    public static String AQUA = COLOR_SYMBOL + "b";
+
+    /**
+     * Represents the color of red
+     */
+    public static String RED = COLOR_SYMBOL + "c";
+
+    /**
+     * Represents the color of light purple
+     */
+    public static String LIGHT_PURPLE = COLOR_SYMBOL + "d";
+
+    /**
+     * Represents the color of yellow
+     */
+    public static String YELLOW = COLOR_SYMBOL + "e";
+
+    /**
+     * Represents the color of white
+     */
+    public static String WHITE = COLOR_SYMBOL + "f";
+}
